0e9ce4bf2c87a32c77173602319308d955216584
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-01-04 12:55:51 -0800

    Add an image to practice adding images to create-react-app projects

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+import dachshundImage from "./../img/dachshundImage.jpeg";
 
 function Header(){
   return (
-    <h1>Help Queue</h1>
+    <React.Fragment>
+      <h1>Help Queue</h1>
+      <img src={dachshundImage} alt="An image of a longhaired dachshund running on a sandy beach at the waters edge" />
+    </React.Fragment>
   );
 }
 
