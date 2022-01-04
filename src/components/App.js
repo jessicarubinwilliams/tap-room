a09e7a0456faf7c45ef8bd82aae3ce5f071e1aa2
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-01-03 18:45:21 -0800

    Import TicketList component and nest it as a child element

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import TicketList from "./TicketList";
 
 function App() {
   const name = "Thato";
@@ -7,10 +8,7 @@ function App() {
   return (
     <React.Fragment>
       <Header />
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      <hr/>
+      <TicketList />
     </React.Fragment>
   );
 }
