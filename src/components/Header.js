dfbf65c4a6af52702572fe3f8b186610102ac630
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-01-05 14:13:10 -0800

    Removed redundant word from img alt attritube

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header(){
   return (
     <React.Fragment>
       <h1>Help Queue</h1>
-      <img src={dachshundImage} alt="An image of a longhaired dachshund running on a sandy beach at the waters edge" />
+      <img src={dachshundImage} alt="A longhaired dachshund running on a sandy beach at the waters edge" />
     </React.Fragment>
   );
 }
