9709034345f5f3c87d3d9bb1662148b89ff6d629
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-01-23 12:38:53 -0800

    Add Ticket propTypes for id and whenTicketClicked

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -16,7 +16,9 @@ function Ticket(props){
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  issue: PropTypes.string
+  issue: PropTypes.string,
+  id: PropTypes.string,
+  whenTicketClicked: PropTypes.func
 };
 
 export default Ticket;
