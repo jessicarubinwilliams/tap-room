3cef9851aae160f7a962f94e073bfb707f23c6b6
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-01-23 11:28:59 -0800

    Add placeholder TicketDetail function component

---
+++ src/components/TicketDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TicketDetails(props) {
+  return (
+    <React.Fragment>
+      <h1>Ticket Detail</h1>
+    </React.Fragment>
+  );
+}
+
+export default TicketDetail;
