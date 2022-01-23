a69d27ec8a236d86fe039382885964334403c13c
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-01-23 12:51:27 -0800

    Update TicketDetail to include props & propTypes

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -1,11 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Ticket from "./Ticket";
 
 function TicketDetail(props) {
+  const { ticket } = props;
+
   return (
     <React.Fragment>
       <h1>Ticket Detail</h1>
+      <h3>{ticket.location} - {ticket.names}</h3>
+      <p><em>{ticket.issue}</em></p>
+      <hr/>
     </React.Fragment>
   );
 }
+
+TicketDetail.propTypes = {
+  ticket: PropTypes.object
+};
 
 export default TicketDetail;
