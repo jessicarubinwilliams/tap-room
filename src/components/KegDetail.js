08e7952d6b60e9e27ebcd6415c242d84a0f183e0
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-01-25 19:51:21 -0800

    UPdate KegDetails: Fix props syntax error & add missing propType

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -9,7 +9,7 @@ function KegDetail(props) {
       <h1>Keg Detail</h1>
       <h3>{keg.name} - {keg.brand}</h3>
       <p>$ {keg.pintPrice} /pint</p>
-      <p>{props.brix} %Brix</p>
+      <p>{keg.brix} %Brix</p>
       <h3>Pints remaining: {keg.pintsRemaining}  </h3>
       <button onClick={ () => onClickingSellAPint(keg) }>Sell A Pint</button>
       <button onClick={ onClickingEdit }>Update Keg</button>
@@ -22,7 +22,8 @@ function KegDetail(props) {
 KegDetail.propTypes = {
   keg: PropTypes.object,
   onClickingDelete: PropTypes.func,
-  onClickingEdit: PropTypes.func
+  onClickingEdit: PropTypes.func,
+  onClickingSellAPint: PropTypes.func
 };
 
 export default KegDetail;
