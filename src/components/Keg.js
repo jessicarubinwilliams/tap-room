ff8ab34a05e53ea207df69593f72b02125f4a30c
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-01-25 18:22:35 -0800

    Update Keg return to display name before brand

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -5,7 +5,7 @@ function Keg(props) {
   return (
     <React.Fragment>
       <div onClick = {() => props.whenKegClicked(props.id)}>
-        <h3>{props.brand} - {props.name}</h3>
+        <h3>{props.name} - {props.brand}</h3>
         <p><em>{props.pintPrice}</em></p>
         <h3>Pints remaining: {props.pintsRemaining}  </h3>
         <hr/>
